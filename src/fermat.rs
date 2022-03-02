2a7bb484b18f05a9130be5620720c9dcf555a3d2
Laika <laika@albina.cc>
2022-03-03 01:56:36 +0900

    Remove isqrt

--- src/fermat.rs
+++ src/fermat.rs
@@ -1,4 +1,4 @@
-use crate::algs::{is_square, isqrt};
+use crate::algs::is_square;
 use crate::traits::{Factor, Factorizer};
 use num::bigint::BigInt;
 
@@ -18,14 +18,14 @@ impl Factorizer for Fermat {
 }
 
 fn factorize(n: &BigInt) -> Option<Vec<Factor>> {
-    let mut x = isqrt(n);
+    let mut x = n.sqrt();
     let mut y2 = x.clone() * x.clone() - n.clone();
     while !is_square(&y2) {
         x += 1u32;
         y2 = x.clone() * x.clone() - n;
     }
-    let a = x.clone() + isqrt(&y2);
-    let b = x.clone() - isqrt(&y2);
+    let a = x.clone() + y2.sqrt();
+    let b = x.clone() - y2.sqrt();
 
     let p1 = Factor::new(a, 1);
     let p2 = Factor::new(b, 1);
